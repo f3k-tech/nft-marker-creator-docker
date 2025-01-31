7ad43edd07e3c73736660ce8b596bc8b3339bbdb
FreekO <info@f3k.tech>
2025-01-31 19:05:23 +0300

    add support for more extensions

--- scripts/process_images.sh
+++ scripts/process_images.sh
@@ -2,14 +2,14 @@
 
 shopt -s nullglob
 
-files=(./input/*.jpg)
+files=(./input/*.{jpg,JPG,jpeg,JPEG,png,PNG}) 
 
 if [ ${#files[@]} -eq 0 ]; then
     echo "No image files found."
 else
     for file in "${files[@]}"; do
         filename="${file##*/}"  # Extract only the filename
-        echo "Processing input/$filename"
+        echo "Processing $filename"
         node app.js -i "input/$filename"
     done
 fi
